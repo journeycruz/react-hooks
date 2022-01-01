e8237366260b0564addee91b29e3eb2f087ac46e
Journey Cruz <journey.cruz@gmail.com>
2021-12-31 18:56:30 -0600

    change typography settings on p tags. New default font-size is 1.1em. Changes to useState documentation made as well (still in progress)

--- client/src/hooks/BasicHooks.js
+++ client/src/hooks/BasicHooks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CodeBlock } from '@atlaskit/code';
+import { CodeBlock, Code } from '@atlaskit/code';
 
 const exampleCodeBlock = `// React Component
 class HelloMessage extends React.Component {
@@ -22,13 +22,13 @@ export const basicHooks = [
     title: 'useState',
     to: 'useState',
     description:
-      'The useState hook was designed to allow functional components access to React State.',
+      'The "State Hook" is used for creating and updating React state in a component without writing a class.',
     body: [
       <div key="1">
         <br />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          The term <Code>state</Code> refers to data or properties that need to
+          be recorded and referenced to throughout an application.
         </p>
         <br />
       </div>,
@@ -44,9 +44,9 @@ export const basicHooks = [
       <div className="note" key="3">
         <h3>Note</h3>
         <span className="note-text">
-          You might be wondering why were using a counter here instead of a
-          more realistic example. This is to help us focus on the API while
-          were still making our first steps with Hooks.
+          You might be wondering why were using a counter here instead of a more
+          realistic example. This is to help us focus on the API while were
+          still making our first steps with Hooks.
         </span>
         <br />
       </div>,
